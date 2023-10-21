795774242b3e43df788744344b6f656debb79627
xnuue <jeromegio604@gmail.com>
2023-10-22 01:47:07 +0800

    tapos n

--- app/src/main/java/com/example/calculator/Login.java
+++ app/src/main/java/com/example/calculator/Login.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +16,33 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button button = findViewById(R.id.button);
         Button button4 = findViewById(R.id.button4);
         EditText et1 = findViewById(R.id.editTextText);
         EditText et2 = findViewById(R.id.editTextTextPassword);
 
-        button4.setOnClickListener(new View.OnClickListener() {
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT);
+                if(et1.getText().toString().equals("admin") && et2.getText().toString().equals("admin")){
+                    Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT). show();
+                    openLogin();
+                } else
+                    Toast.makeText(getApplicationContext(), "Wrong user or pass!", Toast.LENGTH_SHORT). show();
+                    et1.setText("");
+                    et2.setText("");
+
             }
         });
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+    private void openLogin(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -112,10 +113,13 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnLongClickListener(new View.OnLongClickListener(){
             public boolean onLongClick(View view){
                 Toast.makeText(getApplicationContext(), "Goodbye!", Toast.LENGTH_SHORT). show();
-                System.exit(0);
+                exit();
                 return true;
             }
         });
     }
-
+    private void exit(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
 }
